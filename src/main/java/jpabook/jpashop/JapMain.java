4555c7e1e0f8e4d6399601e6db10616164030e0e
danbi <leedanbiii@naver.com>
2024-01-31 15:39:43 +0900

    상속관계 매핑

--- src/main/java/jpabook/jpashop/JapMain.java
+++ src/main/java/jpabook/jpashop/JapMain.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Book;
 
 public class JapMain {
     public static void main(String[] args) {
@@ -15,6 +16,11 @@ public class JapMain {
         tx.begin();
 
         try {
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
+            em.persist(book);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -6,7 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Item {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "DTYPE")
+public abstract class Item {
 
     @Id @GeneratedValue
     @Column(name = "ITEM_ID")
